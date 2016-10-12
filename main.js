767c447a6abeb5c602b443604256fc7a3692fc26
Carmen Chico <CarmenChico@icloud.com>
2016-10-11 20:06:04 -0400

    Added normalize.css file, converted scss to css via sass, and added js/css

---
+++ main.js
@@ -0,0 +1,31 @@
+var d, h, m, s, color;
+
+function displayTime() {
+    d= new Date();
+    h= d.getHours();
+    m= d.getMinutes();
+    s=d.getSeconds();
+
+//this adds a zero in front of the sigle digit hour
+    if(h<=9)h = "0"+h;
+    if(h<=9)m = "0"+m;
+    if(h<=9)s = "0"+s;
+
+
+//set color
+color = "#" + h + m + s;
+
+docmument.body.style.background = color;
+
+//set the time
+document.getElementById("hex").innerHTML = color;
+
+
+//time for change the color by the second
+setTimeout(displayTime, 1000);
+
+}
+
+
+//call the function
+displayTime();
